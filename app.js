9c112bc67439745ceb4fc8fc5fe1cd949af94249
raissaboeng <raissa.boeng@gmail.com>
2021-02-19 21:34:42 -0300

    add header

---
+++ app.js
@@ -0,0 +1,10 @@
+/* Menu mobile - acionamento */
+
+const MenuMobile = {
+    onOffMenu(){
+        document.querySelector('.menu').classList.toggle('active')
+        document.querySelector('.content').classList.toggle('menu-open')
+    }
+
+}
+
